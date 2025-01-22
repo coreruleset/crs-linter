7a237bc4c0436b50ba531c420b7601fa1ce5208c
Felipe Zipitria <felipe.zipitria@owasp.org>
2025-01-22 10:02:31 -0300

    fix: add mocker module
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,7 @@
-from pytest import mocker
-
 from crs_linter.cli import main
 
 
-def test_cli():
+def test_cli(mocker):
     mocker.patch(
         "sys.argv",
         ["-v", "4.10.0", "-r", "../examples/*.conf", "-t", "./APPROVED_TAGS", "-d", "."]
