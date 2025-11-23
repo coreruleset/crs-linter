4cb8fa3d26edad5ee4f1cd03ca710e8bc096187c
Felipe Zipitria <felipe.zipitria@owasp.org>
2025-11-23 17:21:24 +0100

    fix: remove chained
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- src/crs_linter/rules/approved_tags.py
+++ src/crs_linter/rules/approved_tags.py
@@ -4,14 +4,14 @@ from crs_linter.rule import Rule
 
 class ApprovedTags(Rule):
     """Check that only tags from the util/APPROVED_TAGS file are used."""
-    
+
     def __init__(self):
         super().__init__()
         self.success_message = "No new tags added."
         self.error_message = "There are one or more new tag(s)."
         self.error_title = "new unlisted tag"
         self.args = ("data", "tags")
-    
+
     def check(self, data, tags):
         """
         check that only tags from the util/APPROVED_TAGS file are used
@@ -19,8 +19,7 @@ class ApprovedTags(Rule):
         # Skip if no tags list provided
         if tags is None:
             return
-            
-        chained = False
+
         ruleid = 0
         for d in data:
             if "actions" in d:
